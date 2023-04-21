285a6fdd9411fd5d591835fb7ff5dafd05940dce
HuyPham-99 <huypham99.work@gmail.com>
2023-04-21 15:08:26 +0700

    commit

--- EMBEDDED/I2C_PROTOCOL/I2C_Master/I2C_Master.ino
+++ EMBEDDED/I2C_PROTOCOL/I2C_Master/I2C_Master.ino
@@ -1,56 +1,388 @@
 /*
- * File: I2C_Master.ino
+ * File: I2C_MASTER.cpp
  * Author: Hung Huy
- * Date: 4/12/2023
- * Description: Program master I2C Arduino
+ * Date: 4/13/2023
+ * Description: Chương trình master I2C Arduino
  */
+
 /***********Master**************/
-#include <Wire.h>
+#include <Arduino.h>
 
-//Assign macros to Arduno pins to make the code explicit
-#define push 5 //Button
-#define LED1  4 //Led of master
-#define slaveAddress 9 //Address of slave
+/*
+ * Function: MACRO TFull
+ * Description: TFull: chu kỳ clock 
+ */
+#define TFull 50
 
-int buttonState = 0; // variable for reading the pushbutton status
 
-//Function set up 
+//Xác lập địa chỉ cho Slave
+#define SLAVE_ADDRESS 0x55
+
+/*
+ * Function: MACRO SCL_PIN, MACRO SDA_PIN
+ * Description: Định dạng các port theo I2C Arduino
+ */
+#define SCL_PIN A5
+#define SDA_PIN A4
+
+/*
+ * Function: MACRO SCL_OUTPUT, MACRO SDA_OUTPUT, MACRO SDA_INPUT
+ * Description: Set up của các chân SCL và SDA trạng thái nhận hay xuất data
+ */
+#define SCL_OUTPUT pinMode(SCL_PIN, OUTPUT)
+#define SCL_INPUT pinMode(SCL_PIN, INPUT)
+#define SDA_OUTPUT pinMode(SDA_PIN, OUTPUT)
+#define SDA_INPUT pinMode(SDA_PIN, INPUT)
+
+/*
+ * Function: MACRO SCL_HIGH, MACRO SCL_LOW
+ * Description: Set up trạng thái cho SCL là HIGH hoặc LOW
+ */
+#define SCL_HIGH digitalWrite(SCL_PIN, HIGH)
+#define SCL_LOW digitalWrite(SCL_PIN, LOW)
+
+/*
+ * Function: MACRO SDA_HIGH, MACRO SDA_LOW
+ * Description: Set up trạng thái cho SDA là HIGH hoặc LOW
+ */
+#define SDA_HIGH digitalWrite(SDA_PIN, HIGH)
+#define SDA_LOW digitalWrite(SDA_PIN, LOW)
+
+/*
+ * Function: MACRO SDA_READ, MACRO SCL_READ
+ * Description: Đọc data tại SDA và SCL
+ */
+#define SDA_READ digitalRead(SDA_PIN)
+#define SCL_READ digitalRead(SCL_PIN)
+
+
+/*
+* Function: setup
+* Description: Hàm chỉ chạy 1 lần duy nhất khi Arduino hoạt động
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void setup()
 {
-  Serial.begin( 9600 ); // start serial for output
+  delay(100);
 
-  pinMode(push, INPUT);
-
-  Wire.begin(); // join i2c bus (address optional for master) 
+  Serial.begin(9600);
 }
 
-//Function main of program
 /*
- * Function: loop
- * Description: Read status of button and light up LED1 after that transmit data to device #9.
- * Input:
- *   slaveAddress - int
- *   buttonState - int
- * Output:  
- *   LED1 - int
- */
+* Function: loop
+* Description: Hàm chạy lặp đi lặp lại khi Arduino hoạt động
+* - Đọc data từ Slave và truyền data ngược lại cho Slave
+*/
 void loop()
 {
-  Wire.beginTransmission(slaveAddress);// TRANSMIT TO DEVICE #9
+    byte res[10] = {0};
 
-  buttonState = digitalRead(push); //Read status of button
+    int8_t data;
 
-  if (buttonState == HIGH){
-  
-        digitalWrite(LED1,HIGH);
-  }
-  else{
-        digitalWrite(LED1,LOW);
-  }
+    Serial.println("Started write data");
 
-  Wire.write(buttonState);//Write status or vallue of buttonState
+    char c_Data[10] = "1234";
 
-  Wire.endTransmission();//End of transmission
+    Serial.println(String((char*) c_Data));
 
-  delay(100);
+    data = I2C_Writedata(SLAVE_ADDRESS, c_Data);
+
+    Serial.println("Started write data successfully");
+
+    delay(200);
+
+    data = I2C_ReadData(SLAVE_ADDRESS, res, 4);
+
+    Serial.println("Started reading");
+    
+    if (data == 1)
+    {
+      Serial.println("Read data from Slave is ok: ");
+
+      Serial.println(String((char*) res));
+    }
+    else 
+    {
+      Serial.println("Fail to read");
+    }
+    
+    delay(200);
+}
+
+/*
+* Function: I2C_Start
+* Description: Hàm bắt đầu giao tiếp I2C
+* Input:
+*   void
+* Output:
+*   return: None
+*/
+void I2C_Start(void)
+{
+  SDA_HIGH;
+
+  SCL_HIGH;
+	
+  SDA_OUTPUT;
+
+  SCL_OUTPUT;
+  delayMicroseconds(TFull/2);//Delay 1 chu kỳ 
+
+  SDA_LOW;  
+  delayMicroseconds(TFull/2);//Delay 1 chu kỳ 
+
+  SCL_LOW;	
+}
+
+/*
+* Function: I2C_Stop
+* Description: Hàm kết thúc giao tiếp I2C
+* Input:
+*   void
+* Output:
+*   return: None
+*/
+void I2C_Stop(void){	
+	SDA_LOW;
+    delayMicroseconds(TFull/2);//Delay 1 chu kỳ 
+
+    SCL_HIGH;
+	delayMicroseconds(TFull/2);//Delay 1 chu kỳ 	
+
+    SDA_HIGH;
+}
+
+
+/*
+* Function: I2C_ReadAck
+* Description: Hàm đọc tín hiệu ACK hoặc NACK từ Slave để quyết định READ or WRITE
+* Input:
+*   void
+* Output:
+*   return: ACK - uint8_t
+*/
+//Hàm check ACK or NACK
+uint8_t I2C_ReadAck(void){
+
+  uint8_t ACK;
+
+  //READ ACK OR NACK 
+  SDA_INPUT; 
+  delayMicroseconds(TFull/2);
+
+  SCL_HIGH;
+  delayMicroseconds(TFull/2);
+
+  //Đọc giá trị sau đó lưu vào uint8_t ACK
+  ACK = SDA_READ; //Trả về 0 hoặc 1
+
+  SCL_LOW;
+
+  SDA_OUTPUT;
+
+  delayMicroseconds(TFull/2);
+
+  return ACK;  //ACK = 0 hoặc NACK = 1;
+}
+
+/*
+* Function: I2C_Write8bit
+* Description: Hàm truyền 8 bit data từ Master sang Slave
+* Input:
+*   data - byte
+* Output:
+*   No return
+*/
+//Hàm gửi 8 data
+void I2C_Write8bit(byte data){ //Only send 8bit with 8 clock no start/stop/ack
+
+    uint8_t i;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if ((data & 0x80) != 0) //data & 1000.0000 = 1
+        {
+          SDA_HIGH; //Gửi bit cao nhất 1
+        }
+        else
+        {
+          SDA_LOW; //Gửi bit thấp nhất 0
+        }
+
+        data = data << 1;   
+        
+        delayMicroseconds(TFull/2);
+        SCL_HIGH;
+        delayMicroseconds(TFull/2);
+        SCL_LOW;
+    }
+}
+
+/*
+* Function: I2C_Writedata
+* Description: Hàm truyền n data từ Master sang Slave
+* Input:
+*   address - byte
+*   data - char*
+* Output:
+*   No return 1 or -1 (Success or Failure)
+*/
+//Hàm gửi n data
+uint8_t I2C_Writedata(byte address, char* data){
+
+    uint8_t ack;
+
+    //(1) Start
+    I2C_Start();
+    
+    //(2) Start sending data
+    address = address << 1; //7 bit + 0 => write data
+
+    I2C_Write8bit(address);
+
+    //(3) READ ACK OR NACK 
+    ack = I2C_ReadAck();
+
+    if (ack == 1) //ACK == NACK
+    {
+      I2C_Stop();
+
+      return -1;
+    }
+    //(4) Send data until data == 0 
+    else{
+      while (*data != '\0')
+      {
+        I2C_Write8bit(*data);
+          
+        ack = I2C_ReadAck(); //Check ACK
+
+        if (ack == 1) //ACK == NACK
+        {
+          I2C_Stop();
+
+          return -1;
+        }
+        data++;
+      }
+    }
+    //(5) Stop sending
+    I2C_Stop(); 
+
+    return 1;
+}
+
+/*
+* Function: I2C_Read8bit
+* Description: Hàm đọc 8 bit data từ Slave gửi về Master
+* Input:
+*   void
+* Output:
+*   data - uint8_t
+*/
+//Hàm đọc 8 bit data
+byte I2C_Read8bit(void)
+{
+    uint8_t data; 
+
+    int i;
+
+    SDA_INPUT;
+
+    SCL_OUTPUT;
+
+    SCL_LOW;
+
+    //READ 8BIT DATA-----------------------
+    for (int i = 0; i < 8; i++)
+    {
+        delayMicroseconds(TFull/2);
+        
+        SCL_HIGH;
+
+        data = (data << 1) | SDA_READ;
+
+        delayMicroseconds(TFull/2);
+
+        SCL_LOW;
+    }
+
+    return data;
+}
+
+/*
+* Function: I2C_SendAck
+* Description: Hàm gửi tín hiệu ACK hoặc NACK tới Slave
+* Input:
+*   void 
+* Output:
+*   ACK or NACK
+*/
+void I2C_SendAck(void)
+{
+    SCL_LOW;
+
+    SDA_OUTPUT;
+
+    SDA_LOW;//Send ACK
+    delayMicroseconds(TFull/2);
+
+    SCL_HIGH;
+    delayMicroseconds(TFull/2);
+
+    SCL_LOW;
+    
+    SDA_INPUT;
+    delayMicroseconds(TFull/2);
+}
+
+/*
+* Function: I2C_ReadData
+* Description: Hàm đọc n data từ Slave gửi về Master
+* Input:
+*   address - byte
+*   data - byte*
+*   count - byte
+* Output:
+*   No return 1 or -1 (Success or Failure)
+*/
+//Hàm đọc n data
+int8_t I2C_ReadData(byte address, byte* data, byte count){
+
+    int8_t ack;
+    //(1) START --------------------------------
+    I2C_Start();
+
+    //(2) SEND ADDRESS (7bit) + (1 bit READ) --------------------------------
+    address = (address << 1) + 1; //7 bit + 1 => READ data
+
+    I2C_Write8bit(address);
+
+    //(3) Check ACK------------------------------
+    ack = I2C_ReadAck();
+
+    if (ack == 1) //ACK == NACK
+    {
+      I2C_Stop();
+
+      return -1; //FAIL (-1)
+    }
+    //(4) Đọc n data
+    else{
+      for (int i = 0; i < count; i++)
+      {
+        *data = I2C_Read8bit();
+
+        data++; 
+
+        I2C_SendAck();
+      }
+    }
+    //(5) STOP-------------------
+    I2C_Stop();
+    
+    return 1;
 }
--- EMBEDDED/I2C_PROTOCOL/I2C_Slave/I2C_Slave.ino
+++ EMBEDDED/I2C_PROTOCOL/I2C_Slave/I2C_Slave.ino
@@ -1,55 +1,371 @@
 /*
- * File: I2C_Slave.ino
+ * File: I2C_SLAVE.cpp
  * Author: Hung Huy
- * Date: 4/12/2023
- * Description: Program slave I2C Arduino
+ * Date: 4/13/2023
+ * Description: Chương trình slave I2C Arduino
  */
-/***********Slave**************/
-#include <Wire.h>
 
-#define LED2 4 //Led of slave
+/***********SLAVE**************/
+#include <Arduino.h>
 
-#define slaveAddress 9 //Address of slave
 
-int buttonState = LOW; //Set up status of button is LOW
+/*
+ * Function: MACRO TFull
+ * Description: TFull: chu kỳ clock 
+ */
+#define TFull 50
 
-//Function set up 
-void setup()
-{
-  // Start the I2C Bus as Slave on address 9
-  Wire.begin(slaveAddress);
-  
-  Wire.onReceive(receiveEvent); 
+//Xác lập địa chỉ cho Slave
+#define SLAVE_ADDRESS 0x55
 
-  // Define the LED pin as Output
-  pinMode(LED2, OUTPUT);
+/*
+ * Function: MACRO SCL_PIN, MACRO SDA_PIN
+ * Description: Định dạng các port theo I2C Arduino
+ */
+#define SCL_PIN A5
+#define SDA_PIN A4 
+
+/*
+ * Function: MACRO SCL_OUTPUT, MACRO SDA_OUTPUT, MACRO SDA_INPUT
+ * Description: Set up của các chân SCL và SDA trạng thái nhận hay xuất data
+ */
+#define SCL_OUTPUT pinMode(SCL_PIN, OUTPUT)
+#define SCL_INPUT pinMode(SCL_PIN, INPUT)
+#define SDA_OUTPUT pinMode(SDA_PIN, OUTPUT)
+#define SDA_INPUT pinMode(SDA_PIN, INPUT)
+
+/*
+ * Function: MACRO SCL_HIGH, MACRO SCL_LOW
+ * Description: Set up trạng thái cho SCL là HIGH hoặc LOW
+ */
+#define SCL_HIGH digitalWrite(SCL_PIN, HIGH)
+#define SCL_LOW digitalWrite(SCL_PIN, LOW)
+
+/*
+ * Function: MACRO SDA_HIGH, MACRO SDA_LOW
+ * Description: Set up trạng thái cho SDA là HIGH hoặc LOW
+ */
+#define SDA_HIGH digitalWrite(SDA_PIN, HIGH)
+#define SDA_LOW digitalWrite(SDA_PIN, LOW)
+
+/*
+ * Function: MACRO SDA_READ, MACRO SCL_READ
+ * Description: Đọc data tại SDA và SCL
+ */
+#define SDA_READ digitalRead(SDA_PIN)
+#define SCL_READ digitalRead(SCL_PIN)
+
+/*
+* Function: setup
+* Description: Hàm chỉ chạy 1 lần duy nhất khi Arduino hoạt động
+* Input:
+*   void
+* Output:
+*   return: None
+*/
+void setup(){
+
+    Serial.begin(9600);
+    delay(100);
 }
 
 /*
- * Function: receiveEvent
- * Description: Attach a function to trigger when something is received.
- * - Read status of button and light up LED2.
- * Input:
- *   buttonState - int
- * Output:  
- *   LED2 - int
- */
-void receiveEvent(int buttonState)
-{
-  buttonState = Wire.read();
+* Function: loop
+* Description: Hàm chạy lặp đi lặp lại khi Arduino hoạt động
+* - Đọc data từ Master và truyền data ngược lại cho Master
+*/
+/*USE THIS FUNCTION
+- I2C BEGIN(): CHECK START CONDISTIONS , CHECK ADDRESS 
+- I2C WRITE(): SENE CHAR *DATA
+- I2C READ():  READ DATA[] FROM MASTER
+*/
+void loop(){
+    
+    int8_t data;//Condistion data from master
 
-  if (buttonState == LOW){
-  
-        digitalWrite(LED2,HIGH);
-  }
-  else{
-        digitalWrite(LED2,LOW);
-  }
+    int8_t dataFromS;//data from Slave to master
+
+    byte dataFromMaster[10] = {0};
+
+    char dataFromSlave[10] = "1234";
+
+    data = I2C_Begin(SLAVE_ADDRESS);//Đợi tín hiệu từ Master
+
+    if (data == 0)
+    {   
+       I2C_ReadData(dataFromMaster, 4);
+
+       Serial.println("Data from Master is:");
+
+       Serial.println(String((char*) dataFromMaster));
+    }
+    else if(data == 1) //Truyền data cho Master
+    {   
+        
+        dataFromS = I2C_WriteData(dataFromSlave);
+
+        Serial.println("Master want to read data");
+
+        Serial.println(dataFromSlave);
+
+        if (dataFromS == 1)
+        {
+            Serial.println("Write data successful");
+        }
+        else if(dataFromS == -1)
+        {
+            Serial.println("Write data failed");
+        }
+        else{
+            Serial.println("SOMETHING ERROR");
+        }
+    }
+    //Wrong address
+    else if(data == -1){
+        Serial.println("Wrong address");
+    }
+    //Data is uncontroll
+    else{
+        Serial.println("Other number return uncontrol");
+    }
+    delay(200);
 }
 
-//Function main of program
-void loop()
+
+/*
+* Function: I2C_Begin
+* Description: Hàm bắt đầu giao tiếp I2C
+* Input:
+*   address - byte
+* Output:
+*   return:  
+*   (1) Right address
+*           Write: 0
+*           Read: 1
+*   (2) Wrong address: return 2
+*   (3) Not start condistion : loop again
+*/
+uint8_t I2C_Begin(byte address){
+   
+    uint8_t Ack_Read_Write = 0;
+
+    uint8_t data = 0;
+
+    uint8_t i;
+
+    while (true)
+    {
+        SDA_INPUT;
+
+        SCL_INPUT;
+
+        while ((SDA_READ != 1 || SCL_READ != 1)); //Đợi tín hiệu từ Master (start condition)
+
+        while (SDA_READ == 1); //Đợi tín hiệu từ SDA_READ về 0 chuẩn bị bắt đầu quá trình đọc dữ liệu từ Master 
+
+        delayMicroseconds(1); //Delay nữa chu kỳ
+
+        if (SCL_READ == 1)// Start condition
+        {
+            while (SCL_READ == 1); //Đợi tín hiệu SCL về 0
+
+            for (int i = 0; i < 8; i++)
+            {
+                while (SCL_READ == 0); //Đợi tín hiệu SCL về 1
+            
+                data = (data << 1) | SDA_READ; 
+
+                while (SCL_READ == 1); //Đợi tín hiệu SCL về 0
+            }
+            
+            //Gía trị của ACK or NACK
+            Ack_Read_Write =  data & 1;
+
+            data = data >> 1;
+
+            if (data == address)
+            {
+                I2C_SendAck();
+
+                return Ack_Read_Write;
+            }
+            else{
+                return -1;
+            }
+        }
+        else
+        {
+            //Do nothing if address is not true
+        }
+    }
+    
+}
+
+/*
+* Function: I2C_Read8bit
+* Description: Hàm đọc 8 bit data từ Master gửi về Slave
+* Input:
+*   void
+* Output:
+*   data - uint8_t
+*/
+uint8_t I2C_Read8bit(void){
+
+    uint8_t i;
+
+    uint8_t data;
+
+    SDA_INPUT;
+
+    SCL_INPUT;
+
+    for (int i = 0; i < 8; i++)
+    {
+        while (SCL_READ == 0); //Đợi tín hiệu SCL về 1
+            
+        data = (data << 1) | SDA_READ; 
+
+        while (SCL_READ == 1); //Đợi tín hiệu SCL về 0
+    }
+    //Đọc 1 byte data thành công
+
+    return data;
+}
+
+/*
+* Function: I2C_ReadData
+* Description: Hàm đọc n data từ Slave gửi về Master
+* Input:
+*   dataFromMaster - byte*
+*   count - uint8_t
+* Output:
+*   data - byte
+*/
+int8_t I2C_ReadData(byte* dataFromMaster, uint8_t count){
+
+    for (int i = 0; i < count; i++)
+    {
+        *dataFromMaster = I2C_Read8bit();
+
+        dataFromMaster++;   
+
+        I2C_SendAck();
+    }
+}
+
+/*
+* Function: I2C_Write8bit
+* Description: Hàm truyền 8 bit data từ Slave sang Master
+* Input:
+*   data - byte
+* Output:
+*   No return
+*/
+//Send 8 bit (1 byte data) to Master
+void I2C_Write8Data(byte data){
+
+    SDA_OUTPUT;
+
+    uint8_t i;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if((data & 0x80) != 0)
+        {
+            SDA_HIGH;
+        }
+        else
+        {
+            SDA_LOW;
+        }
+
+        data = data << 1;
+
+        while (SCL_READ == 0);//Đợi tín hiệu SCL từ master
+
+        while (SCL_READ == 1);//Đợi tín hiệu SCL từ master  
+
+    }
+    SDA_INPUT;
+}
+
+/*
+* Function: I2C_ReadAck
+* Description: Hàm đọc tín hiệu ACK hoặc NACK từ Master để quyết định READ or WRITE
+* Input:
+*   void
+* Output:
+*   return: ACK - uint8_t
+*/
+//Hàm check ACK or NACK của Slave
+uint8_t I2C_ReadAck(void){
+
+  uint8_t ACK;
+
+  //READ ACK OR NACK 
+  SDA_INPUT; 
+  delayMicroseconds(TFull/2);
+   
+  while (SCL_READ == 0);//Đợi tín hiệu SCL từ master
+
+  //Đọc giá trị sau đó lưu vào uint8_t ACK
+  ACK = SDA_READ; //Trả về 0 hoặc 1
+
+  while (SCL_READ == 1);//Đợi tín hiệu SCL từ master
+
+  return ACK;  //ACK = 0 hoặc NACK = 1;
+}
+
+/*
+* Function: I2C_SendAck
+* Description: Hàm gửi tín hiệu ACK hoặc NACK tới Master
+* Input:
+*   void 
+* Output:
+*   ACK or NACK
+*/
+void I2C_SendAck(void)
 {
-  delay(100);
+    SDA_OUTPUT;
+
+    SDA_LOW; //Send ACK to Master
+
+    while (SCL_READ == 0); //Đợi tín hiệu SCL về 1
+
+    while (SCL_READ == 1); //Đợi tín hiệu SCL về 0
+
+    SDA_INPUT;  
+}
+
+
+/*
+* Function: I2C_WriteData
+* Description: Hàm gửi n data từ Slave gửi về Master
+* Input:
+*   data - char*
+* Output:
+*   No return 1 or -1 (Success or Failure)
+*/
+//Send n data from Slave to master
+uint8_t I2C_WriteData(char *data)
+{
+    uint8_t ack;
+
+    while (*data != '\0')
+    {
+        I2C_Write8Data(*data);
+
+        ack = I2C_ReadAck();
+        if (ack == 1)
+        {
+            SDA_INPUT;
+
+            return ack;
+        }
+        data++;  
+    }
+
+    SDA_INPUT;
+    return 1;
 }
 
